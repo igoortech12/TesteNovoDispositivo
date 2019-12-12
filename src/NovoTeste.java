3bac7efa1c953a4c70b6fc4c1b87a9dab39f0b3a
Igor Oliveira <igoortech@gmail.com>
2019-12-12 00:32:43 -0300

    De novo2

--- src/NovoTeste.java
+++ src/NovoTeste.java
@@ -3,8 +3,7 @@ public class NovoTeste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Aprendendo a usar o Git");
-		System.out.println("De novo!");
+		System.out.println("De novo! E de novo!");
 	}
 
 }
